4191b00aa7b880d4a02655fafb99948859b555c4
Narasimha <narasimhaskarry@gmail.com>
2025-05-13 14:31:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Chatbot from "./chatbot";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
